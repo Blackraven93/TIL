8b465d9e11b4b30cf656ea28bf7f429a071774ff
raven <reblackraven@gmail.com>
2022-01-11 22:33:12 +0900

    Refactor: format 함수 제거

--- src/JavaScript/refactoring/Chapter1.js
+++ src/JavaScript/refactoring/Chapter1.js
@@ -26,11 +26,11 @@ performancesMake("hamlet", 55);
 performancesMake("as-like", 35);
 performancesMake("othello", 40);
 
-function format(aNumber) {
+function krw(aNumber) {
   return new Intl.NumberFormat("ko-KR", {
     style: "currency",
     currency: "KRW",
-  }).format(aNumber);
+  }).format(aNumber / 100);
 }
 
 function volumeCreditsFor(aPerformance) {
@@ -78,13 +78,13 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력
-    result += `    ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `    ${playFor(perf).name}: ${krw(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${krw(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result.trim();
 }
