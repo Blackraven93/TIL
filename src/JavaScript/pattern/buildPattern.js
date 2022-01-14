e7d8dbc4374310439fae22e15f9aaeab39345add
kwak se jin <Reblackraven@gmail.com>
2022-01-14 22:32:58 +0900

    Javascript: builder pattern

--- src/JavaScript/pattern/buildPattern.js
+++ src/JavaScript/pattern/buildPattern.js
@@ -7,13 +7,35 @@ class Address {
 
 
 class User {
-    constructor(name, age, phone, address) {
+    constructor(name) {
         this.name = name
-        this.age = age
-        this.phone = phone
-        this.address = address
     }
 }
 
-const user = new User("Bob", undefined, undefined, new Address("1", "shade tree"))
+class UserBuilder {
+    constructor(name) {
+        this.user = new User(name)
+    }
+
+    setAge(age) {
+        this.user.age = age
+        return this
+    }
+
+    setPhone(phone) {
+        this.user.phone = phone
+        return this
+    }
+
+    setAge(address) {
+        this.user.address = address
+        return this
+    }
+
+    build() {
+        return this.user
+    }
+}
+
+let user = new UserBuilder('Bob').setAge(10).setPhone('010-1111-1111').build()
 console.log(user)
