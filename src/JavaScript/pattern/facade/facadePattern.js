6db627c016880b38baaa6eaa0e1cb9c0416bdb1f
kwak se jin <Reblackraven@gmail.com>
2022-01-15 15:39:45 +0900

    Javascript: after facade pattern

--- src/JavaScript/pattern/facade/facadePattern.js
+++ src/JavaScript/pattern/facade/facadePattern.js
@@ -1,20 +1,14 @@
 async function getUsers() {
-    return await fetch(`https://jsonplaceholder.typicode.com/users`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-    }).then(res => res.json())
+    return await getFetch(`https://jsonplaceholder.typicode.com/users`)
 }
 
 async function getUserPosts(userId) {
-    return await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-    }).then(res => res.json())
+    return await getFetch(`https://jsonplaceholder.typicode.com/posts`, { userId })
 }
+
 
 getUsers().then(users => {
     users.forEach(user => {
-
         getUserPosts(user.id).then(posts => {
             console.log(posts)
             console.log(user.name)
@@ -22,3 +16,21 @@ getUsers().then(users => {
         })
     })
 })
+
+async function getFetch(url, params = {}) {
+    const queryString = Object.entries(params).map(param => `${param[0]}=${param[1]}`).join('&')
+    return await fetch(`${url}?${queryString}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+    }).then(res => res.json())
+}
+
+/** Axios
+ * function getFetch(url, params = {}) {
+ * return axios({
+ *      url:url,
+ *      method: "GET",
+ *      params: params
+ *  }).then(res => res.data)
+ * }
+ */
