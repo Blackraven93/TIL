9f6f30f2ddf73b26177bd6836599f46578ec9076
Blackraven93 <reblackraven@gmail.com>
2022-01-17 09:40:38 +0900

    Javascript: create sample mini lib

---
+++ src/JavaScript/library/library.js
@@ -0,0 +1,23 @@
+const $ = (selector) => {
+    const self = {
+        element: document.querySelector(selector),
+        html: () => self.element,
+        on: (event, callback) => {
+            document.addEventListener(event, callback) 
+        },
+        hide: () => {
+            self.element.style.display = "none";
+        },
+        attr: (name, value) => {
+            if (value == null) {
+                self.element.getAttribute(name)
+            } else {
+                self.element.setAttribute(name, value)
+            }
+        }
+    }
+
+    return self
+}
+
+$('h2').attr('class', 'hellothere')
