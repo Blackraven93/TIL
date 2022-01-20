afa86a986e9a8f28535e283591cd7326a996cce9
Blackraven93 <reblackraven@gmail.com>
2022-01-20 20:10:01 +0900

    Pattern: study callback pattern

--- src/Pattern/chapterFour.js
+++ src/Pattern/chapterFour.js
@@ -1,3 +1,5 @@
+
+
 // 기명 함수 표현식
 var add = function add(a, b) {
   return a + b;
@@ -31,3 +33,47 @@ console.log(bird2.name);
 console.log(bird3.name);
 
 // 책이랑 다르게 전부다 나온다.
+
+function bar() {
+  return 'global bar'
+}
+
+function hoistMe() {
+  console.log(typeof bar) 
+  // bar() Error
+  
+  var bar = function () {
+    return 'local bar'
+  };
+}
+
+console.log(bar())
+console.log(hoistMe()); // undefined Error가 나지 않는다 왜? 호이스팅 때문에
+
+// 콜백
+
+// 콜백 없이 시작
+
+var findNodes = function() {
+  var i = 100000,
+    nodes = [];
+
+  while (i) {
+    i -= 1;
+    nodes.push(i);
+  }
+
+  return nodes;
+}
+
+console.log(findNodes())
+
+var hide = function(nodes) {
+  var i = 0,
+    max = nodes.length;
+    for (; i < max; i += 1) {
+      nodes[i].style.display = "none";
+    }
+};
+
+hide(findNodes())
