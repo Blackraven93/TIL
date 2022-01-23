db6daa5ac8db31fa8d3f67d20a9c1a8cf844431b
raven <reblackraven@gmail.com>
2022-01-23 23:24:09 +0900

    Vue: vue test

--- src/JavaScript/iterable/iterable.js
+++ src/JavaScript/iterable/iterable.js
@@ -140,7 +140,8 @@ function printTodo(todo) {
 }
 printTodo({ id: 1, content: "HTML", completed: true }); // 할일 HTML은 완료 상태입니다.
 
-function printTodo({ content, completed }) {
+function printTodo2({ content, completed }) {
   console.log(`할일 ${content}은 ${completed ? "완료" : "비완료"} 상태입니다.`);
 }
-printTodo({ id: 1, content: "HTML", completed: true });
+
+printTodo2({ id: 1, content: "HTML", completed: true });
