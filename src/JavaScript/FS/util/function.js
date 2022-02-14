ba5a995be312c14a4a41f1859584aca697ec148c
raven <reblackraven@gmail.com>
2022-02-15 00:00:50 +0900

    Docs: 순수 함수 readme 수정

--- src/JavaScript/FS/util/function.js
+++ src/JavaScript/FS/util/function.js
@@ -1,15 +1,14 @@
-const forEach = (array, fn) => {
+export const forEach = (array, fn) => {
   if (Array.isArray(array)) {
     let index;
     for (index = 0; index < array.length; index++) {
-      array[index] = fn(array[index]);
+      fn(array[index]);
     }
-    return array;
   }
 };
 
 const arr = [1, 2, 3, 4, 5];
 
-const newArr = forEach(arr, (e) => e * e);
+const newArr = forEach(arr, (e) => console.log(e * e));
 
-console.log(arr); // 원본 함수의 변화
+console.log(newArr); // 원본 함수의 변화
