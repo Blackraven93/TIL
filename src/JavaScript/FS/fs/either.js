f7f51039ee1db4b6acec629cec417928ac4cc7c7
Blackraven93 <reblackraven@gmail.com>
2022-03-18 23:58:43 +0900

    Javascript: Either 예시 공부

--- src/JavaScript/FS/fs/either.js
+++ src/JavaScript/FS/fs/either.js
@@ -7,7 +7,7 @@ Nothing.of = function(val) {
 };
 
 Nothing.prototype.map = function(f) {
-  return this;
+  return f(this);
 }
 
 const Some = function(val) {
@@ -18,3 +18,76 @@ Some.of = function(val) {
   return new Some(val);
 }
 
+Some.prototype.map = function(fn) {
+  return Some.of(fn(this.value))
+}
+
+console.log(Some.of('test').map(x => x.toUpperCase()))
+
+// Nothing.of('test').map(x => x.toUpperCase())
+
+class Either {
+  constructor(value){
+    this.$value = value;
+  }
+
+  static right(value) {
+    return new Right(value)
+  }
+
+  static left(value) {
+    return new Left(value);
+  }
+}
+
+class Right extends Either {
+  get isRight() {
+    return true;
+  }
+
+  get isLeft() {
+    return false
+  }
+
+  map(fn) {
+    return new Right(fn(this.$value));
+  }
+}
+
+class Left extends Either {
+  get isRight() {
+    return false;
+  }
+
+  get isLeft() {
+    return true;
+  }
+
+  map(fn) {
+    return this;
+  }
+}
+
+const books = [
+  { id: 'book1', title: 'coding with javascript', author: 'Chris Minnick, Eva Holland' },
+  { id: 'book2', title: 'speaking javaScript', author: 'Axel Rauschmayer' },
+];
+
+
+const logByEitherStatus = (eitherBook) => {
+  return eitherBook.isLeft 
+    ? console.error(`Author: ${eitherBook.$value.author}`) 
+    : console.log(`Author: ${eitherBook.$value.author}`)
+}
+
+const logBookAuthor = (bookId, books) => {
+  return pipe(
+    findBookById(bookId),
+    validateBookAuthor,
+    logByEitherStatus
+  )(books)
+};
+
+
+logBookAuthor('book1', books);
+logBookAuthor('book2', books); 
