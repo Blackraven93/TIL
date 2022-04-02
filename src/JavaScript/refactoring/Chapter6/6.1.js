91b3f2f09de0c77baba401fc166a89620a0dc82e
Blackraven93 <reblackraven@gmail.com>
2022-04-02 23:34:45 +0900

    Javascript: Refactoring 6장 6.1 함수 추출하기

---
+++ src/JavaScript/refactoring/Chapter6/6.1.js
@@ -0,0 +1,36 @@
+const obj = {
+  customer : 'raven'
+}
+
+const calculateOutstanding = () => {
+  return 1_000_000
+}
+
+const printBanner = () => {}
+
+const printOwing = invoice => {
+  printBanner()
+  let outstanding = calculateOutstanding();
+
+  console.log(`고객명: ${invoice.customer}`)
+  console.log(`채무액: ${outstanding}`);
+}
+
+const refactoring = invoice => {
+  const printDetails = (outstanding)  => {
+    console.log(`고객명: ${invoice.customer}`)
+    console.log(`채무액: ${outstanding}`);  
+  }
+  
+  printBanner()
+  let outstanding = calculateOutstanding();
+  printDetails(outstanding)
+
+  // 함수 선언으로 하면 하위에 작성 불가
+  // function printDetails (outstanding)  {
+  //   console.log(`고객명: ${invoice.customer}`)
+  //   console.log(`채무액: ${outstanding}`);  
+  // }
+}
+
+refactoring(obj)
