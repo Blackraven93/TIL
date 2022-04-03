a8797ae5e057c1db0e3b9c65313d9e5e1ec0c156
Blackraven93 <reblackraven@gmail.com>
2022-04-03 23:50:22 +0900

    Javascript: 객체 리터럴 패턴을 이용한 순수함수 예제

--- src/JavaScript/FS/fs/basic.js
+++ src/JavaScript/FS/fs/basic.js
@@ -12,3 +12,52 @@ longRunningFnBookKeeper.hasOwnProperty(ip)
 
 console.log(longRunningFnBookKeeper);
 
+
+// 내부 접근 차당
+const zipCode = (code, location) => {
+  let _code = code;
+  let _location = location || '';
+
+  return {
+    code: function() {
+      return _code;
+    },
+    location: function() {
+      return _location
+    },
+    fromString: function(str) {
+      let parts = str.split('-')
+      return zipCode(parts[0], parts[1]);
+    },
+    toString: function() {
+      return _code + '-' + _location;
+    }
+  }
+}
+
+const princetonZip = zipCode('02', '444-333');
+console.log(princetonZip.toString())
+
+const coordinate = (lat, long) => {
+  let _lat = lat
+  let _long = long
+
+  return {
+    latitude: function() {
+      return _lat;
+    },
+    longitude: function() {
+      return _long;
+    },
+    translate: function(dx, dy) {
+      return coordinate(_lat + dx, _long + dy)
+    },
+    toString: function() {
+      return `(${_lat}, ${_long})`
+    }
+  }
+}
+
+const greenwich = coordinate(51.4778, 0.0015);
+console.log(greenwich.toString())
+console.log(greenwich.translate(10, 10).toString())
