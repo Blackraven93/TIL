6462031aa7f16e3860868a75d1627023d94f9a03
Blackraven93 <reblackraven@gmail.com>
2022-04-14 00:22:59 +0900

    Javascript: 비공개 멤버의 필요성

--- src/Pattern/object/createPattern.js
+++ src/Pattern/object/createPattern.js
@@ -55,3 +55,65 @@ console.log(module3 === MYAPP.modules.module2);
 console.log(MYAPP.namespace('modules.module51'));
 
 console.log(MYAPP.namespace('once.upon.a.time.there.was.this.long.nested'));
+
+const myFunction = function () {
+  const event = YAHOO.util.Event,
+    dom = YAHOO.util.Dom;
+};
+
+const myobj = {
+  myprop: 1,
+  getProp: function () {
+    return this.myprop;
+  },
+};
+
+console.log(myobj.myprop);
+console.log(myobj.getProp());
+
+function Gadget() {
+  this.name = 'iPod';
+  this.stretch = function () {
+    return 'iPad';
+  };
+}
+
+const toy = new Gadget();
+console.log(toy.name);
+console.log(toy.stretch());
+
+class Gadget2 {
+  #name = 'iPod';
+
+  get getName() {
+    return this.#name;
+  }
+}
+
+const toy2 = new Gadget2();
+console.log(`toy2 name : ${toy2.name}`);
+console.log(toy2.getName);
+
+function Gadget3() {
+  const name = 'iPod';
+  this.getName = function () {
+    return name;
+  };
+}
+
+const toy3 = new Gadget3();
+
+console.log(`toy3 name : ${toy3.name}`);
+console.log(`toy3 getName : ${toy3.getName()}`);
+
+function Gadget4() {
+  const specs = {
+    screen_width: 320,
+    screen_height: 480,
+    color: 'white',
+  };
+
+  this.getSpecs = function () {
+    return specs;
+  };
+}
