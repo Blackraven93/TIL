f9633aa20e14fc1e8990bdd3629c6c4f17235f96
Blackraven93 <reblackraven@gmail.com>
2022-07-15 21:50:20 +0900

    Javascript: 함수형 코딩 목업 데이터

---
+++ src/JavaScript/FS/functionalPractice.js
@@ -0,0 +1,53 @@
+const users = [
+  {
+    email: 'john@coldmail.com',
+    rec_count: 2,
+  },
+  {
+    email: 'sam@pmail.co',
+    rec_count: 16,
+  },
+  {
+    email: 'linda1989@oal.com',
+    rec_count: 1,
+  },
+  {
+    email: 'jan1940@ahoy.com',
+    rec_count: 0,
+  },
+  {
+    email: 'mrbig@pmail.co',
+    rec_count: 25,
+  },
+  {
+    email: 'lol@lol.lol',
+    rec_count: 0,
+  },
+];
+
+const coupons = [
+  {
+    coupon: 'MAYDISCOUNT',
+    rank: 'good',
+  },
+  {
+    coupon: '10PERCENT',
+    rank: 'bad',
+  },
+  {
+    coupon: 'PROMOTION45',
+    rank: 'best',
+  },
+  {
+    coupon: 'IHEARTYOU',
+    rank: 'bad',
+  },
+  {
+    coupon: 'GETADEAL',
+    rank: 'best',
+  },
+  {
+    coupon: 'ILIKEDISCOUNTS',
+    rank: 'good',
+  },
+];
